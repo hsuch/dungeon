b4472dc83c00a078de63835a1adb75b9ba9a0e68
Qi Jin <qijin@Qis-MacBook-Pro.local>
2018-10-11 16:16:24 -0500

    Add Animation Class

--- src/edu/virginia/engine/display/Animation.java
+++ src/edu/virginia/engine/display/Animation.java
@@ -1,4 +1,43 @@
 package edu.virginia.engine.display;
 
+import java.util.ArrayList;
+
 public class Animation {
+
+    private String id;
+
+    private Integer startFrame;
+
+    private Integer endFrame;
+
+    private String fileName;
+
+    public Animation(String id, Integer startFrame, Integer endFrame) {
+        this.setId(id);
+        this.setStartFrame(startFrame);
+        this.setEndFrame(endFrame);
+    }
+
+    public String getId() {
+        return this.id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getStartFrame() {
+        return this.startFrame;
+    }
+    public void setStartFrame(Integer startFrame) {
+        this.startFrame = startFrame;
+    }
+
+    public Integer getEndFrame() {
+        return this.endFrame;
+    }
+    public void setEndFrame(Integer endFrame) {
+        this.endFrame = endFrame;
+    }
+
+
 }
