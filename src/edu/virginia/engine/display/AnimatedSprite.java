04303935efdbfe807f799d7f91e0cbf3e57ac604
hsuch <hsuch@uchicago.edu>
2018-10-11 16:58:31 -0500

    fix images, more animated sprite logic

--- src/edu/virginia/engine/display/AnimatedSprite.java
+++ src/edu/virginia/engine/display/AnimatedSprite.java
@@ -1,11 +1,8 @@
 package edu.virginia.engine.display;
 
 import java.awt.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
-import java.util.stream.Stream;
 
 import edu.virginia.engine.util.GameClock;
 
@@ -14,10 +11,8 @@ public class AnimatedSprite extends Sprite {
 
     private Boolean playing;
 
-    private String fileName;
-
     /* Assuming images will be specified by filename */
-    private ArrayList<String> frames;
+    private ArrayList<BufferedImage> frames;
 
     private int currentFrame;
 
@@ -34,8 +29,12 @@ public class AnimatedSprite extends Sprite {
     public AnimatedSprite(String id, String fileName, Point position) {
         super(id);
         this.fileName = fileName;
-        this.animationSpeed = DEFAULT_ANIMATION_SPEED; // idk just a number lol
+        this.animationSpeed = DEFAULT_ANIMATION_SPEED;
         this.gameClock = new GameClock();
+        this.playing = true;
+        this.currentFrame = 0;
+        this.startFrame = 0;
+        this.endFrame = 0;
     }
 
     public void initGameClock() {
@@ -52,15 +51,23 @@ public class AnimatedSprite extends Sprite {
         }
     }
 
-    public void importSprites(Animation anim) {
-        String dir = anim.getFileName();
-        try (Stream<Path> paths = Files.walk(Paths.get("resources"))) {
-            paths.filter(Files::isRegularFile)
-                    .forEach(frames)
-        }
+    public void importSprites() {
+        this.frames.add(super.readImage("0000.png"));
+        this.frames.add(super.readImage("0001.png"));
+        this.frames.add(super.readImage("0002.png"));
+        this.frames.add(super.readImage("0003.png"));
+        this.frames.add(super.readImage("0004.png"));
+        this.frames.add(super.readImage("0005.png"));
+        this.frames.add(super.readImage("0006.png"));
+        this.frames.add(super.readImage("0007.png"));
+        this.frames.add(super.readImage("mario1.png"));
+        this.frames.add(super.readImage("mario2.png"));
+        this.frames.add(super.readImage("mario3.png"));
     }
 
     public void animate(Animation anim) {
+        this.startFrame = anim.getStartFrame();
+        this.endFrame = anim.getEndFrame();
     }
 
     public void animate(String id) {
@@ -74,7 +81,8 @@ public class AnimatedSprite extends Sprite {
     }
 
     public void stopAnimation(int frameNumber) {
-        
+        this.startFrame = frameNumber;
+        this.endFrame = frameNumber;
     }
 
     public void stopAnimation() {
